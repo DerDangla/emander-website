4d4b3ab56d67f5f03365a9825d5f0cc021171403
DerDangla <emanderdangla@gmail.com>
2023-09-16 13:03:45 -0400

    update main page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,18 +2,11 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+    <main className="flex min-h-screen flex-col items-center justify-between p-20 border-2 border-rose-800">
       
-    <div className="mb-6 grid text-center lg:max-w-7xl lg:w-full lg:mt-10 lg:grid-cols-3 lg:text-left md:mt-10 sm:mt-10">
-      <Image
-        className="col-span-2 md:mx-auto md:block md:col-span-2 lg:mx-0 lg:float-left lg:col-span-1 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        src="../emander-photo.jpg"
-        alt="Emander Photo"
-        width={600}
-        height={400}
-        priority
-      />
-      <p className={`col-span-2 mt-2 lg:mt-0 lg:ml-10 md:mt-2 md:ml-0`}>
+    <div className="mb-6 grid text-center lg:max-w-7xl lg:w-full lg:mt-10 lg:grid-cols-3 lg:text-left md:mt-10 sm:mt-10 border-2 border-rose-800">
+      
+      <div className={`col-span-2 mt-2 lg:mt-0 lg:ml-10 md:mt-2 md:ml-0`}>
         <h1 className={`mb-3 text-4xl font-semibold`}> Hi There !</h1>
         <p className="mb-3 text-lg">
         My name is Emander, I am working towards completing a post-secondary degree in Artificial Intelligence - Software Engineering and have over 6 years of hands-on experience in software development and application support. I am passionate
@@ -26,7 +19,7 @@ export default function Page() {
           <li>Studying for ITIL 4 Foundation certification</li>
           <li>Looking for interesting and impactful work related to Application Support or AI (please reach out)</li>
         </ul>
-      </p>
+      </div>
     </div>
   </main>
   )
